858580bacebe934f1cfbc078d795489d17f481e3
Yiyum <fuyiyu1999@outlook.com>
2023-04-16 03:10:10 +0800

    dadad

--- TodoList/TodoList/Models/ItemModel.swift
+++ TodoList/TodoList/Models/ItemModel.swift
@@ -21,7 +21,7 @@ struct ItemModel: Identifiable, Codable{
 	
 	func updateCompletion() -> ItemModel {
 		return ItemModel(id: id, title: title, isCompleted: !isCompleted)
-        print("ddd")
+        
         //print("ddd")
 	}
 }
